39f277bd44532a80739f9183bc5edcd854deae95
Kolesnichenko <Kolesnichenko@KOLESNICHENKOOM>
2019-11-15 13:10:57 +0200

    remove files

--- BP2019_2020/SIT1/JivkoJelqzkov/1.HomeWork/fn19621754_prob3.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-	int a = 576;
-	cout << a % 10 << (a % 100) / 10 << a / 100;
-	return 0;
-}
--- BP2019_2020/SIT1/RadoslavIvanov/1.HomeWork/fn19621667_prob3.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "The number 576 has:" << 576 % 10 << " units, " << 576 / 10 % 10 << " tens, " << 576 / 100 << " hundreds ";
-	return 0;
-}
